bbcca80166dd2dc183f13171a1e088cc6ecc8945
Zooheck <davidscottflack@gmail.com>
2019-02-22 12:16:13 -0600

    Add new actions

--- helpers/actionHelpers.js
+++ helpers/actionHelpers.js
@@ -1,1 +1,25 @@
 const db = require('../dbConfig.js')
+
+module.exports = {
+    add
+}
+
+// async function getById(id) {
+//     const action = await db('actions')
+//         .where('project_id', '=', id)
+//         .select('description', 'notes', 'completed')
+//     const project = 
+// }
+function getById(id) {
+    return db('actions')
+        .where({ id })
+        .first()
+}
+
+function add(action) {
+    return db('actions')
+        .insert(action)
+        .then(ids => {
+            return getById(ids[0]);
+        })
+}
--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const ActionFuncs = require('../helpers/actionHelpers.js')
+
+router.post('/', async (req, res) => {
+    if (!req.body.project_id || !req.body.description || !req.body.notes) {
+        return res.status(404).json({ message: 'Your action must include a project_id, a description, and notes'})
+    }
+    const newAction = await ActionFuncs.add(req.body)
+
+    try {
+        res.status(201).json(newAction)
+    } catch (error) {
+        res.status(500).json({ message: 'Error adding project'})
+    }
+})
 module.exports = router;
